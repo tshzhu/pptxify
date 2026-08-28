002c024130200f27d1aad4fa0ae1b8a98a0cf2a3
tshzhu <tshzhu@gmail.com>
2026-08-29 06:55:34 +0800

    chore: simplify CLI package preparation

--- scripts/add-cli-extensions.mjs
+++
@@ -1,7 +0,0 @@
-import { readFile, writeFile } from 'node:fs/promises';
-
-for (const file of ['dist-cli/core.js', 'dist-cli/pptx-notes.js']) {
-  const text = await readFile(file, 'utf8');
-  const updated = text.replace(/from '(\.\/[^']+?)(?<!\.js)'/g, "from '$1.js'");
-  await writeFile(file, updated);
-}
